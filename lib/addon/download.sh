2c19a973fc68edcf9110fe23aab036b39065f98e
Kamontat Chantrachirathumrong <14089557+kamontat@users.noreply.github.com>
2023-12-06 23:17:46 +0700

    perf: update plugin from template [autocommit]

---
+++ lib/addon/download.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+## Get download mode based on input filename
+## usage: `kc_asdf_download_mode 'test.tar.gz'`
+## output: git|file|archive|package|custom
+kc_asdf_download_mode() {
+  local ns="download-mode.addon"
+  local filename="$1"
+  local mode="file"
+
+  if [ -z "$filename" ]; then
+    mode="custom"
+  elif echo "$filename" | grep -qiE "\.git$"; then
+    mode="git"
+  fi
+
+  kc_asdf_debug "$ns" "download mode of %s is %s" \
+    "$filename" "$mode"
+  printf "%s" "$mode"
+}
